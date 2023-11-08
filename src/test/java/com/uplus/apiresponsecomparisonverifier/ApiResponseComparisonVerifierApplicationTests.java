0d7b43ef23bdf1fd2d5a6e774ec67705321c5c33
손형규/메타버스플랫폼개발팀 <144748810+hks0902@users.noreply.github.com>
2023-11-08 14:53:31 +0900

    first commit

---
+++ src/test/java/com/uplus/apiresponsecomparisonverifier/ApiResponseComparisonVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplus.apiresponsecomparisonverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResponseComparisonVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
